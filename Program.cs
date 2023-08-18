5f5dcedb70a2fd3889140393bca9be22e0f2c9d6
Santiago Rojas <santirojastr12@gmail.com>
2023-08-18 15:29:49 -0500

    Configuracion de endpoints 'get' para el modelo de AlumnoAsignatura

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddSqlServer<UniversidadContext>(builder.Configuration.GetConne
 
 var app = builder.Build();
 
+#region GetAlumnos
 app.MapGet("/", async ([FromServices] UniversidadContext context) =>
 {
     context.Database.EnsureCreated();
@@ -29,5 +30,28 @@ app.MapGet("/get/alumnos/doc/{documento}", async ([FromServices] UniversidadCont
 {
     return Results.Ok(context.Alumnos.Where(e => e.AlumnoDocumento == documento));
 });
+#endregion
+
+#region GetAlumnoAsignaturas
+app.MapGet("/get/alumnoasignaturas", async ([FromServices] UniversidadContext context) =>
+{
+    return Results.Ok(context.AlumnoAsignaturas.Include(e => e.Alumno).Include(e => e.Asignatura));
+});
+
+app.MapGet("/get/alumnoasignaturas/alumno/{id}", async ([FromServices] UniversidadContext context, [FromRoute] Guid id) =>
+{
+    return Results.Ok(context.AlumnoAsignaturas.Where(e => e.AlumnoId == id).Include(e => e.Alumno).Include(e => e.Asignatura));
+});
+
+app.MapGet("/get/alumnoasignaturas/asignatura/{id}", async ([FromServices] UniversidadContext context, [FromRoute] Guid id) =>
+{
+    return Results.Ok(context.AlumnoAsignaturas.Where(e => e.AsignaturaId == id).Include(e => e.Alumno).Include(e => e.Asignatura));
+});
+
+app.MapGet("/get/alumnoasignaturas/alumno/{idAlumno}/asignatura/{idAsignatura}", async ([FromServices] UniversidadContext context, [FromRoute] Guid idAlumno, [FromRoute] Guid idAsignatura) =>
+{
+    return Results.Ok(context.AlumnoAsignaturas.Where(e => e.AlumnoId == idAlumno && e.AsignaturaId == idAsignatura).Include(e => e.Alumno).Include(e => e.Asignatura));
+});
+#endregion
 
 app.Run();
